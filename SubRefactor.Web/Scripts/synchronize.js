2049ec1804d976e6943f6c1cd393e6494b6fe932
Dyego Costa <dyego.costa@live.com>
2011-08-04 16:10:19 -0300

    Separating the javascript from the pages to files

---
+++ SubRefactor.Web/Scripts/synchronize.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    
+    $(function() {
+        $("#slider-range-min").slider({
+                range: "min",
+                value: 0,
+                min: -10000,
+                max: 10000,
+                slide: function(event, ui) {
+                    $("#Delay").val(ui.value);
+                }
+            });
+        $("#Delay").val($("#slider-range-min").slider("value"));
+        $("#Delay").change(function() {
+            $("#slider-range-min").slider("value", this.value);
+        });
+    });
+    
+});
---
+++ SubRefactor.Web/Scripts/translate.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    $("#Translator").change(function () {
+        if ($(this).val() != null && $(this).val() != "") {
+            listLanguages($(this).val());
+        }
+        else {
+            $("#FromLanguage :gt(0)").remove();
+            $("#ToLanguage :gt(0)").remove();
+        }
+    });
+
+});
+
+function listLanguages(translator) {
+    $("#FromLanguage :gt(0)").remove();
+    $("#ToLanguage :gt(0)").remove();
+    $.getJSON('@Url.Content("~/Views/Subtitle/ListLanguagesByTranslator")/' + translator, listLanguagesCallBack);
+}
+
+function listLanguagesCallBack(json) {
+    $(json).each(function () {
+        $("#FromLanguage").append("<option value='" + this.Value + "'>" + this.Key + "</option>");
+        $("#ToLanguage").append("<option value='" + this.Value + "'>" + this.Key + "</option>");
+    });
+}
