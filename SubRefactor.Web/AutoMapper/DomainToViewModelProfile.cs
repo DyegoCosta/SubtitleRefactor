79646e681c56da9aed5541e80cb7b509ed603128
Dyego Costa <dyego.costa@live.com>
2011-09-27 22:53:42 -0300

    Configuring AutoMapper

---
+++ SubRefactor.Web/AutoMapper/DomainToViewModelProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SubRefactor.Models.Account;
+
+namespace SubRefactor.AutoMapper
+{
+    public class DomainToViewModelProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelProfile"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Domain.User, RegisterViewModel>();
+            Mapper.CreateMap<Domain.User, LogOnViewModel>();
+        }
+    }
+}
---
+++ SubRefactor.Web/AutoMapper/ViewModelToDomainProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Security;
+using AutoMapper;
+using SubRefactor.Domain;
+using SubRefactor.Models.Account;
+
+namespace SubRefactor.AutoMapper
+{
+    public class ViewModelToDomainProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainProfile"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<RegisterViewModel, User>()
+                .ForMember(u => u.Password,
+                           m => m.MapFrom(
+                               source =>
+                               FormsAuthentication.HashPasswordForStoringInConfigFile(source.Password, "MD5")));
+
+            Mapper.CreateMap<LogOnViewModel, User>()
+                .ForMember(u => u.Password,
+                           m =>
+                           m.MapFrom(
+                               source =>
+                               FormsAuthentication.HashPasswordForStoringInConfigFile(source.Password, "MD5")));
+        }
+    }
+}
