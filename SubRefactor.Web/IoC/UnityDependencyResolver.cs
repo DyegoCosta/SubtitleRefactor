0944a2abbe7ca40c5e6263090a8c6753cd6b9308
Dyego Costa <dyego.costa@live.com>
2011-09-27 22:51:02 -0300

    IoC

---
+++ SubRefactor.Web/IoC/UnityDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+
+namespace SubRefactor.IoC
+{
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        #region IDependencyResolver Members
+
+        public object GetService(Type serviceType)
+        {
+            if (!_container.IsRegistered(serviceType))
+                if (serviceType.IsAbstract || serviceType.IsInterface)
+                    return null;
+
+            return _container.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.ResolveAll(serviceType);
+        }
+
+        #endregion
+    }
+}
