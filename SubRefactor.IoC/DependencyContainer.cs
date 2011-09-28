2de729944963c140941fcaa1cbb668e2258c57f2
Dyego Costa <dyego.costa@live.com>
2011-09-27 22:49:11 -0300

    Implementing the container

---
+++ SubRefactor.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Unity;
+using SubRefactor.IRepository;
+using SubRefactor.IRepository.Infrastructure;
+using SubRefactor.Repository.Infrastructure;
+using SubRefactor.Repository.Repositories;
+
+namespace SubRefactor.IoC
+{
+    public class DependencyContainer
+    {
+        public static IUnityContainer GetUnityContainer()
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<IDatabaseFactory, DatabaseFactory>(new HttpContextLifetimeManager<IDatabaseFactory>());
+            container.RegisterType<IUnitOfWork, UnitOfWork>(new HttpContextLifetimeManager<IUnitOfWork>());
+            container.RegisterType<IUserRepository, UserRepository>(new HttpContextLifetimeManager<IUserRepository>());            
+            
+            return container;
+        }
+    }
+}
